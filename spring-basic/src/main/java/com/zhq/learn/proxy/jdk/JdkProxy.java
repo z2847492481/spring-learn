0cc67340fc4e3140a84d253418efebdd9b1450ba
zhqzhq <2847492481@qq.com>
2024-07-21 20:51:00 +0800

    补充反射优化

--- spring-basic/src/main/java/com/zhq/learn/proxy/jdk/JdkProxy.java
+++ spring-basic/src/main/java/com/zhq/learn/proxy/jdk/JdkProxy.java
@@ -11,6 +11,7 @@ public class JdkProxy {
 
         $Proxy0 proxy = new $Proxy0((method, args1) -> {
             System.out.println("before...");
+            // 反射调用优化：16次反射调用之后，会优化为直接调用
             return method.invoke(target, args1);
         });
 
